9be408263729b2f5a4212e68e9ad766a427a6036
CeoDevForce <ceodevforce@gmail.com>
2022-05-10 19:22:24 -0500

    added dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        '@nuxtjs/color-mode',
+        '@vueuse/nuxt',
+        '@unocss/nuxt',
+        '@pinia/nuxt'
+    ],
+    
 })
